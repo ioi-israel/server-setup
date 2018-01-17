0efb2c553ee07883bc48bab74ee10515663886ef
Nir Lavee <nir692007@gmail.com>
2018-01-17 18:05:31 +0200

    Use static methods in AutoSetup.

--- auto/AutoSetup.py
+++ auto/AutoSetup.py
@@ -11,7 +11,7 @@ import sys
 import requests
 
 
-class Printer():
+class Log():
     """
     An object for interacting with the user, including colored printing
     and prompts.
@@ -24,32 +24,37 @@ class Printer():
         "red":      "\033[91m"
     }
 
-    def pretty_print(self, text, modifier, end='\n'):
+    @staticmethod
+    def pretty_print(text, modifier, end='\n'):
         """
         Print the given text with the given modifier (color),
         then return to normal mode.
         """
-        print(modifier, text, self.modifiers["normal"], sep="", end=end)
+        print(modifier, text, Log.modifiers["normal"], sep="", end=end)
 
-    def info(self, text, end='\n'):
+    @staticmethod
+    def info(text, end='\n'):
         """
         Print the given text in green.
         """
-        self.pretty_print(text, self.modifiers["green"], end=end)
+        Log.pretty_print(text, Log.modifiers["green"], end=end)
 
-    def fail(self, text, end='\n'):
+    @staticmethod
+    def fail(text, end='\n'):
         """
         Print the given text in red.
         """
-        self.pretty_print(text, self.modifiers["red"], end=end)
+        Log.pretty_print(text, Log.modifiers["red"], end=end)
 
-    def warn(self, text, end='\n'):
+    @staticmethod
+    def warn(text, end='\n'):
         """
         Print the given text in yellow.
         """
-        self.pretty_print(text, self.modifiers["yellow"], end=end)
+        Log.pretty_print(text, Log.modifiers["yellow"], end=end)
 
-    def prompt(self, question, options, default=None):
+    @staticmethod
+    def prompt(question, options, default=None):
         """
         Prompt to choose between several options, with optional default.
         The user can input an entire option or just the first letter.
@@ -74,8 +79,8 @@ class Printer():
                 break
         return choice
 
-    def prompt_dir(self, question, must_exist, must_not_exist,
-                   create, default=None):
+    @staticmethod
+    def prompt_dir(question, must_exist, must_not_exist, create, default=None):
         """
         Prompt to choose a directory (absolute path).
         """
@@ -108,7 +113,8 @@ class Printer():
 
         return choice
 
-    def prompt_password(self, question):
+    @staticmethod
+    def prompt_password(question):
         """
         Prompt for a password, without echoing it to the terminal.
         """
@@ -124,13 +130,17 @@ class Runner():
     An object for executing actions in the system.
     """
 
-    def set_interact(self, interact):
+    interact = 2
+
+    @staticmethod
+    def set_interact(interact):
         """
         Set the interactivity level.
         """
-        self.interact = interact
+        Runner.interact = interact
 
-    def run_step(self, user_index, step, num_steps):
+    @staticmethod
+    def run_step(user_index, step, num_steps):
         """
         Run a given step (action). The step is a dictionary containing
         the fields "text" and "function", which are the description
@@ -142,7 +152,7 @@ class Runner():
         """
         info("[%d/%d: %s]" % (user_index, num_steps, step["text"]))
 
-        if self.interact > 1:
+        if Runner.interact > 1:
             choice = prompt("Continue?", ["yes", "no", "skip"])
             if choice == "skip":
                 info("[Skipping step %d]" % (user_index,))
@@ -168,7 +178,8 @@ class Runner():
 
         return success
 
-    def run(self, commands, fail_abort=True):
+    @staticmethod
+    def run(commands, fail_abort=True):
         """
         Run the described commands list. If fail_abort is True,
         and the return code is not zero, raise an exception.
@@ -179,7 +190,7 @@ class Runner():
         command_string = " ".join(commands)
         warn("[*** Executing: %s *** ]" % command_string)
 
-        if self.interact > 2:
+        if Runner.interact > 2:
             choice = prompt("Continue?", ["yes", "no", "skip"])
             if choice == "skip":
                 info("[Skipping this execution]")
@@ -195,7 +206,8 @@ class Runner():
         warn("[*** Done executing: %s *** ]" % command_string)
         return return_code
 
-    def run_with_io(self, commands, input_str=None, fail_abort=True):
+    @staticmethod
+    def run_with_io(commands, input_str=None, fail_abort=True):
         """
         Run the described commands list, with the given input. If fail_abort
         is True, and the return code is not zero, raise an exception.
@@ -215,7 +227,8 @@ class Runner():
         # Decode the bytes to a normal string.
         return stdout.decode(), return_code
 
-    def write(self, path, text, sudo=False):
+    @staticmethod
+    def write(path, text, sudo=False):
         """
         Write the given text to the given path.
         Use sudo if the flag is given.
@@ -232,7 +245,8 @@ class Runner():
         run(["rm", temp_path])
         return True
 
-    def generate_key(self):
+    @staticmethod
+    def generate_key():
         """
         Generate a 16 byte hex key using cmscommon.crypto.
         Can be used after CMS is installed.
@@ -248,7 +262,8 @@ class Installer():
     An object for installation and configuration of components.
     """
 
-    def install_custom_ubuntu_deps(self):
+    @staticmethod
+    def install_custom_ubuntu_deps():
         """
         Install custom dependencies.
         """
@@ -275,7 +290,8 @@ class Installer():
         run(["sudo", "apt-get", "install"] + packages)
         return True
 
-    def install_ohmyzsh(self):
+    @staticmethod
+    def install_ohmyzsh():
         """
         Install zsh and oh-my-zsh.
         """
@@ -305,7 +321,8 @@ class Installer():
         run(["sudo", "apt-get", "install", "source-highlight"])
         return True
 
-    def setup_custom_config(self):
+    @staticmethod
+    def setup_custom_config():
         """
         Download and replace each file in the custom_config_files
         dictionary. It is assumed each value is a dictionary with fields
@@ -321,7 +338,8 @@ class Installer():
             run(["wget", url, "-O", path])
         return True
 
-    def install_cms_deps(self):
+    @staticmethod
+    def install_cms_deps():
         """
         Install the CMS Ubuntu dependencies with apt.
         """
@@ -361,7 +379,8 @@ class Installer():
         run(["sudo", "apt-get", "install"] + packages)
         return True
 
-    def define_cms_dir(self):
+    @staticmethod
+    def define_cms_dir():
         """
         Define Github related directories, like /home/ioi/Github/ioi-israel/cms
         If the Github directory is not defined yet, prompt for it.
@@ -377,34 +396,37 @@ class Installer():
             os.makedirs(repo_dir)
         cms_dir = os.path.join(repo_dir, "cms")
 
-    def change_to_cms_dir(self):
+    @staticmethod
+    def change_to_cms_dir():
         """
         Change the working directory to the CMS directory.
         """
         os.chdir(cms_dir)
 
-    def clone_cms(self):
+    @staticmethod
+    def clone_cms():
         """
         Clone CMS into the defined CMS directory.
         Checkout the defined CMS branch and isolate branch.
         """
-        self.define_cms_dir()
+        Installer.define_cms_dir()
         run(["git", "clone", "--recursive", cms_url, cms_dir])
-        self.change_to_cms_dir()
+        Installer.change_to_cms_dir()
         run(["git", "checkout", cms_branch])
         os.chdir("isolate")
         run(["git", "checkout", isolate_branch])
         return True
 
-    def run_cms_prerequisites(self):
+    @staticmethod
+    def run_cms_prerequisites():
         """
         Run the CMS prerequisites script.
         Among other things, this adds the user to the cmsuser group.
         If the user has not logged out and back in to apply this,
         let them know and stop.
         """
-        self.define_cms_dir()
-        self.change_to_cms_dir()
+        Installer.define_cms_dir()
+        Installer.change_to_cms_dir()
         script_path = os.path.join(cms_dir, "prerequisites.py")
         run(["sudo", script_path, "install"])
 
@@ -416,29 +438,32 @@ class Installer():
             return False
         return True
 
-    def install_cms_python_deps(self):
+    @staticmethod
+    def install_cms_python_deps():
         """
         Install CMS Python dependencies using pip2.
         """
-        self.define_cms_dir()
-        self.change_to_cms_dir()
+        Installer.define_cms_dir()
+        Installer.change_to_cms_dir()
         requirements_path = os.path.join(cms_dir, "requirements.txt")
         run(["sudo", "pip2", "install", "-r", requirements_path])
         dev_requirements_path = os.path.join(cms_dir, "dev-requirements.txt")
         run(["sudo", "pip2", "install", "-r", dev_requirements_path])
         return True
 
-    def run_cms_setup(self):
+    @staticmethod
+    def run_cms_setup():
         """
         Run the CMS setup script.
         """
-        self.define_cms_dir()
-        self.change_to_cms_dir()
+        Installer.define_cms_dir()
+        Installer.change_to_cms_dir()
         script_path = os.path.join(cms_dir, "setup.py")
         run(["sudo", "python2", script_path, "install"])
         return True
 
-    def setup_cms_db(self):
+    @staticmethod
+    def setup_cms_db():
         """
         Create the database and its user.
         """
@@ -463,12 +488,13 @@ class Installer():
         run(["sudo", "su", "-", "postgres", "-c", commands_str])
         return True
 
-    def customize_cms_config(self):
+    @staticmethod
+    def customize_cms_config():
         """
         Download various CMS configuration templates from this repository,
         modify them as needed and install them to the appropriate paths.
         """
-        self.define_cms_dir()
+        Installer.define_cms_dir()
         choice = prompt("Override cms.conf, cms.ranking.conf, nginx.conf?",
                         ["yes", "no", "skip"])
         if choice == "skip":
@@ -525,21 +551,23 @@ class Installer():
 
         # Run the prerequisites again, in order to install the
         # new CMS configuration files.
-        return self.run_cms_prerequisites()
+        return Installer.run_cms_prerequisites()
 
-    def swap_off(self):
+    @staticmethod
+    def swap_off():
         """
         Turn off swap in the system.
         """
         run(["sudo", "swapoff", "-a"])
         return True
 
-    def cms_test(self):
+    @staticmethod
+    def cms_test():
         """
         Initialize the database, run the CMS test suite, then drop it.
         """
-        self.define_cms_dir()
-        self.change_to_cms_dir()
+        Installer.define_cms_dir()
+        Installer.change_to_cms_dir()
 
         choice = prompt("Warning: init the CMS database?",
                         ["yes", "no"])
@@ -559,14 +587,16 @@ class Installer():
 
         return True
 
-    def cms_init_db(self):
+    @staticmethod
+    def cms_init_db():
         """
         Initialize the CMS database (after it's created).
         """
         run(["cmsInitDB"])
         return True
 
-    def cms_add_admin(self):
+    @staticmethod
+    def cms_add_admin():
         """
         Add a user to the cmsAdminWebServer.
         """
@@ -575,7 +605,8 @@ class Installer():
         run(["cmsAddAdmin", aws_usr, "-p", aws_password])
         return True
 
-    def install_gitolite(self):
+    @staticmethod
+    def install_gitolite():
         """
         Install gitolite3.
         """
@@ -584,20 +615,17 @@ class Installer():
 
 
 # Put common functionality in the global scope, for less cluttered use.
-log = Printer()
-info = log.info
-fail = log.fail
-warn = log.warn
-prompt = log.prompt
-prompt_dir = log.prompt_dir
-prompt_password = log.prompt_password
+info = Log.info
+fail = Log.fail
+warn = Log.warn
+prompt = Log.prompt
+prompt_dir = Log.prompt_dir
+prompt_password = Log.prompt_password
 
-runner = Runner()
-run = runner.run
-run_with_io = runner.run_with_io
-write = runner.write
-generate_key = runner.generate_key
-installer = Installer()
+run = Runner.run
+run_with_io = Runner.run_with_io
+write = Runner.write
+generate_key = Runner.generate_key
 
 # Local paths.
 home_dir = os.path.expanduser("~")
@@ -635,35 +663,35 @@ custom_config_files = {
 # Installation steps. Each has a description and a corresponding function.
 steps = [
     {"text": "Installing custom Ubuntu packages",
-     "function": installer.install_custom_ubuntu_deps},
+     "function": Installer.install_custom_ubuntu_deps},
     {"text": "Installing zsh, and oh-my-zsh from Github",
-     "function": installer.install_ohmyzsh},
+     "function": Installer.install_ohmyzsh},
     {"text": "Downloading custom config files",
-     "function": installer.setup_custom_config},
+     "function": Installer.setup_custom_config},
     {"text": "Installing CMS Ubuntu dependencies",
-     "function": installer.install_cms_deps},
+     "function": Installer.install_cms_deps},
     {"text": "Cloning CMS",
-     "function": installer.clone_cms},
+     "function": Installer.clone_cms},
     {"text": "Running CMS prerequisites",
-     "function": installer.run_cms_prerequisites},
+     "function": Installer.run_cms_prerequisites},
     {"text": "Installing CMS Python dependencies",
-     "function": installer.install_cms_python_deps},
+     "function": Installer.install_cms_python_deps},
     {"text": "Running CMS setup",
-     "function": installer.run_cms_setup},
+     "function": Installer.run_cms_setup},
     {"text": "Creating database user",
-     "function": installer.setup_cms_db},
+     "function": Installer.setup_cms_db},
     {"text": "Customizing CMS and server config",
-     "function": installer.customize_cms_config},
+     "function": Installer.customize_cms_config},
     {"text": "Turning off swap",
-     "function": installer.swap_off},
+     "function": Installer.swap_off},
     {"text": "Running CMS tests (may take a while)",
-     "function": installer.cms_test},
+     "function": Installer.cms_test},
     {"text": "Initializing CMS database",
-     "function": installer.cms_init_db},
+     "function": Installer.cms_init_db},
     {"text": "Adding CMS admin.",
-     "function": installer.cms_add_admin},
+     "function": Installer.cms_add_admin},
     {"text": "Installing gitolite",
-     "function": installer.install_gitolite},
+     "function": Installer.install_gitolite},
 ]
 
 
@@ -709,11 +737,11 @@ def main():
     if args.interact is not None:
         interact = int(args.interact)
 
-    runner.set_interact(interact)
+    Runner.set_interact(interact)
 
     # Run all requested steps, stop on failure.
     for index in range(start_range, end_range):
-        success = runner.run_step(index + 1, steps[index], len(steps))
+        success = Runner.run_step(index + 1, steps[index], len(steps))
         if not success:
             return 1
     return 0
